9811a2dd10645a46135a0f026436589b8245c61b
B131R1N1I <mateuszbernart@gmail.com>
2021-01-30 21:04:50 +0100

    User as Uint
    Code is prettier and simpler because user is represented as uint (id - not by User object as before)

--- flashcards_server/src/DatabaseManagement/DatabaseManagement.Set.cs
+++ flashcards_server/src/DatabaseManagement/DatabaseManagement.Set.cs
@@ -10,7 +10,7 @@ namespace flashcards_server.DatabaseManagement
         public void AddSetToDatabase(Set.Set set)
         {
             using (var cmd = new NpgsqlCommand($"INSERT INTO sets (name, creator_id, owner_id, created_date, last_modification, is_public) " +
-            $"VALUES ('{set.name}', {set.creator.id}, {set.owner.id}, '{(NpgsqlTypes.NpgsqlDateTime)set.createdDate}', " +
+            $"VALUES ('{set.name}', {set.creator}, {set.owner}, '{(NpgsqlTypes.NpgsqlDateTime)set.createdDate}', " +
             $"'{(NpgsqlTypes.NpgsqlDateTime)set.lastModificationDate}', {set.isPublic});", conn))
             {
                 try
@@ -27,34 +27,20 @@ namespace flashcards_server.DatabaseManagement
         public List<Set.Set> GetSetsByOwner(User.User user)
         {
             var listOfSets = new List<Set.Set>();
-            var listOfSetsTemp = new List<Object[]>();
             using (var cmd = new NpgsqlCommand($"SELECT * FROM sets WHERE owner_id = {user.id};", conn))
             {
-                try
+                using (var reader = cmd.ExecuteReader())
                 {
-
-                    using (var output = cmd.ExecuteReader())
+                    if (!reader.HasRows)
+                        return listOfSets;
+                    while (reader.Read())
                     {
+                        var temp = new Object[7];
+                        reader.GetValues(temp);
+                        listOfSets.Add(new Set.Set((String)reader[1], Convert.ToUInt32(reader.GetInt32(2)), Convert.ToUInt32(reader.GetInt32(3)), DateTime.Parse(reader[4].ToString()), DateTime.Parse(reader[5].ToString()), (bool)reader[6], (Int32)reader[0]));
 
-                        while (output.Read())
-                        {
-                            var temp = new Object[7];
-                            output.GetValues(temp);
-                            listOfSetsTemp.Add(temp);
-                        }
                     }
-                    foreach (var i in listOfSetsTemp)
-                    {
-                        listOfSets.Add(new Set.Set((string)i[1], GetUserById((int)i[2]), GetUserById((int)i[3]), (DateTime)i[4], (DateTime)i[5], (bool)i[6], (int)i[0]));
-                    }
-
                 }
-                catch (PostgresException e)
-                {
-                    System.Console.WriteLine("Cannot read any user from DB");
-                    System.Console.WriteLine(">>>" + e.MessageText);
-                }
-
             }
             return listOfSets;
         }
@@ -62,68 +48,42 @@ namespace flashcards_server.DatabaseManagement
         public List<Set.Set> GetSetsByCreator(User.User user)
         {
             var listOfSets = new List<Set.Set>();
-            var listOfSetsTemp = new List<Object[]>();
             using (var cmd = new NpgsqlCommand($"SELECT * FROM sets WHERE creator_id = {user.id};", conn))
             {
-                try
+                using (var reader = cmd.ExecuteReader())
                 {
-
-                    using (var output = cmd.ExecuteReader())
+                    if (!reader.HasRows)
+                        return listOfSets;
+                    while (reader.Read())
                     {
-                        if (!output.HasRows)
-                            return listOfSets;
-                        while (output.Read())
-                        {
-                            var temp = new Object[7];
-                            output.GetValues(temp);
-                            listOfSetsTemp.Add(temp);
-                        }
-                    }
-                    foreach (var i in listOfSetsTemp)
-                    {
-                        listOfSets.Add(new Set.Set((string)i[1], GetUserById((int)i[2]), GetUserById((int)i[3]), DateTime.Parse(i[4].ToString()), DateTime.Parse(i[5].ToString()), (bool)i[6], (int)i[0]));
-                    }
+                        var temp = new Object[7];
+                        reader.GetValues(temp);
+                        listOfSets.Add(new Set.Set((String)reader[1], Convert.ToUInt32(reader.GetInt32(2)), Convert.ToUInt32(reader.GetInt32(3)), DateTime.Parse(reader[4].ToString()), DateTime.Parse(reader[5].ToString()), (bool)reader[6], (Int32)reader[0]));
 
+                    }
                 }
-                catch (PostgresException e)
-                {
-                    System.Console.WriteLine("Cannot read any user from DB");
-                    System.Console.WriteLine(">>>" + e.MessageText);
-                }
-
             }
             return listOfSets;
         }
 
         public Set.Set GetSetByName(String name)
         {
-            Set.Set set;
             using (var cmd = new NpgsqlCommand($"SELECT * FROM sets where name='{name}' LIMIT 1;", conn))
             {
                 Object[] setTemp = new Object[7];
-                try
+
+                using (var reader = cmd.ExecuteReader())
                 {
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        if (!reader.HasRows)
-                            throw new NpgsqlException($"(set)No row found by name: {name}");
-                        reader.Read();
-                        for (int i = 0; i < 7; i++)
-                            setTemp[i] = reader[i];
-                    }
-                    set = new Set.Set((String)setTemp[1], GetUserById((Int32)setTemp[2]), GetUserById((Int32)setTemp[3]), DateTime.Parse(setTemp[4].ToString()), DateTime.Parse(setTemp[5].ToString()), (bool)setTemp[6], (Int32)setTemp[0]);
-                }
-                catch (NpgsqlException)
-                {
-                    throw;
+                    if (!reader.HasRows)
+                        throw new NpgsqlException($"(set)No row found by name: {name}");
+                    reader.Read();
+                    return new Set.Set((String)reader[1], Convert.ToUInt32(reader.GetInt32(2)), Convert.ToUInt32(reader.GetInt32(3)), DateTime.Parse(reader[4].ToString()), DateTime.Parse(reader[5].ToString()), (bool)reader[6], (Int32)reader[0]);
                 }
             }
-            return set;
         }
 
         public Set.Set GetSetById(int id)
         {
-            var setTemp = new Object[7];
             using (var cmd = new NpgsqlCommand($"SELECT * FROM sets WHERE id = {id};", conn))
             {
                 using (var reader = cmd.ExecuteReader())
@@ -131,11 +91,9 @@ namespace flashcards_server.DatabaseManagement
                     if (!reader.HasRows)
                         throw new NpgsqlException($"No set found by id {id}");
                     reader.Read();
-                    for (int i = 0; i < 7; i++)
-                        setTemp[i] = reader[i];
+                    return new Set.Set((String)reader[1], Convert.ToUInt32(reader.GetInt32(2)), Convert.ToUInt32(reader.GetInt32(3)), DateTime.Parse(reader[4].ToString()), DateTime.Parse(reader[5].ToString()), (bool)reader[6], (Int32)reader[0]);
                 }
             }
-            return new Set.Set((String)setTemp[1], GetUserById((Int32)setTemp[2]), GetUserById((Int32)setTemp[3]), DateTime.Parse(setTemp[4].ToString()), DateTime.Parse(setTemp[5].ToString()), (bool)setTemp[6], (Int32)setTemp[0]);
         }
 
         public void TransferOwnership(Set.Set set, User.User user)
