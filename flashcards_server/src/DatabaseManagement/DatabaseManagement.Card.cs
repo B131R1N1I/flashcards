d26070390c19c116e24ed1d04321886cc9aa5e1e
B131R1N1I <mateuszbernart@gmail.com>
2021-02-04 22:28:52 +0100

    GetCardsBySet
    Can get cards by set and fixed GetCardByID - returned question as answer and answer as question

--- flashcards_server/src/DatabaseManagement/DatabaseManagement.Card.cs
+++ flashcards_server/src/DatabaseManagement/DatabaseManagement.Card.cs
@@ -1,5 +1,6 @@
 using System;
 using Npgsql;
+using System.Collections.Generic;
 
 namespace flashcards_server.DatabaseManagement
 {
@@ -11,7 +12,7 @@ namespace flashcards_server.DatabaseManagement
             using (var cmd = new NpgsqlCommand("INSERT INTO cards (question, answer, picture, in_set)" +
             // $"VALUES ('{card.question}', '{card.answer}', @Image, {card.inSet});", conn))
             $"VALUES ('{card.question}', '{card.answer}', @Image, {card.inSet});", conn))
-                
+
             {
                 System.Console.WriteLine(cmd.Parameters);
                 NpgsqlParameter parameter = cmd.CreateParameter();
@@ -23,9 +24,10 @@ namespace flashcards_server.DatabaseManagement
                 cmd.ExecuteNonQuery();
             }
         }
+
         public Card.Card GetCardByID(uint id)
         {
-            using (var cmd = new NpgsqlCommand($"SELECT id, question, answer, picture, in_set, picture is null AS picture_is_null FROM cards WHERE id = {id};", conn))
+            using (var cmd = new NpgsqlCommand($"SELECT id, question, answer, picture, in_set, picture FROM cards WHERE id = {id};", conn))
             {
                 using (var reader = cmd.ExecuteReader())
                 {
@@ -36,12 +38,32 @@ namespace flashcards_server.DatabaseManagement
                         var len = reader.GetBytes(3, 0, null, 0, 0);
                         var buffer = new Byte[len];
                         reader.GetBytes(3, 0, buffer, 0, (int)len);
-                        return new Card.Card(reader.GetString(1), reader.GetString(2), buffer, (uint)reader.GetInt32(4), (uint)reader.GetInt32(0));
+                        return new Card.Card(reader.GetString(2), reader.GetString(1), buffer, (uint)reader.GetInt32(4), (uint)reader.GetInt32(0));
                     }
                     else
-                        return new Card.Card(reader.GetString(1), reader.GetString(2), null, (uint)reader.GetInt32(4), (uint)reader.GetInt32(0));
+                        return new Card.Card(reader.GetString(2), reader.GetString(1), null, (uint)reader.GetInt32(4), (uint)reader.GetInt32(0));
 
 
+                }
+            }
+        }
+
+        public List<Card.Card> GetCardsBySet(Set.Set set)
+        {
+            var listOfCards = new List<Card.Card>();
+            using (var cmd = new NpgsqlCommand($"SELECT * FROM cards WHERE in_set = {set.id};", conn))
+            {
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+
+                    {
+                        var len = reader.GetBytes(3, 0, null, 0, 0);
+                        var buffer = new Byte[len];
+                        reader.GetBytes(3, 0, buffer, 0, (int)len);
+                        listOfCards.Add(new Card.Card(reader.GetString(2), reader.GetString(1), buffer, (uint)reader.GetInt32(4), (uint)reader.GetInt32(0)));
+                    }
+                    return listOfCards;
                 }
             }
         }
