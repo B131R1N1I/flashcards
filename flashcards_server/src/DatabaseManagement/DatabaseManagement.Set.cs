905662973c97daaadfb0f77e7b92091c68757679
B131R1N1I <mateuszbernart@gmail.com>
2021-02-06 20:01:54 +0100

    Make set paused
    Allows to set set as not active - paused

--- flashcards_server/src/DatabaseManagement/DatabaseManagement.Set.cs
+++ flashcards_server/src/DatabaseManagement/DatabaseManagement.Set.cs
@@ -66,6 +66,27 @@ namespace flashcards_server.DatabaseManagement
             return listOfSets;
         }
 
+        // public List<Set.Set> GetActiveSetsByCreator(User.User user)
+        // {
+        //     var listOfSets = new List<Set.Set>();
+        //     using (var cmd = new NpgsqlCommand($"SELECT * FROM sets JOIN active_sets ON sets.id = active_sets WHERE sets.creator_id = {user.id} AND active_sets.is_paused = false;", conn))
+        //     {
+        //         using (var reader = cmd.ExecuteReader())
+        //         {
+        //             if (!reader.HasRows)
+        //                 return listOfSets;
+        //             while (reader.Read())
+        //             {
+        //                 var temp = new Object[7];
+        //                 reader.GetValues(temp);
+        //                 listOfSets.Add(new Set.Set(reader.GetString(1), Convert.ToUInt32(reader.GetInt32(2)), Convert.ToUInt32(reader.GetInt32(3)), reader.GetDateTime(4), reader.GetDateTime(5), reader.GetBoolean(6), reader.GetInt32(0)));
+
+        //             }
+        //         }
+        //     }
+        //     return listOfSets;
+        // }
+
         public Set.Set GetSetByName(String name)
         {
             using (var cmd = new NpgsqlCommand($"SELECT * FROM sets where name='{name}' LIMIT 1;", conn))
@@ -97,6 +118,19 @@ namespace flashcards_server.DatabaseManagement
         public void TransferOwnership(Set.Set set, User.User user)
         {
             using (var cmd = new NpgsqlCommand($"UPDATE sets SET owner_id = {user.id} WHERE id={set.id};", conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void MakeSetPaused(Set.Set set, User.User user)
+        {
+            using (var cmd = new NpgsqlCommand($"DELETE FROM active_sets WHERE user_id = {user.id} AND set_id = {set.id}", conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+            using (var cmd = new NpgsqlCommand($"INSERT INTO active_sets (user_id, set_id, is_paused) VALUES ({user.id}, {set.id}, true);", conn))
             {
                 cmd.ExecuteNonQuery();
             }
