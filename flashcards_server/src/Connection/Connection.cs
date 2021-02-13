387159da1dc462cdd8416d6c3b5453a28bd8c212
B131R1N1I <mateuszbernart@gmail.com>
2021-02-13 20:27:03 +0100

    Created Connection

---
+++ flashcards_server/src/Connection/Connection.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+using System.Text.Json;
+using System.Net.Sockets;
+using System.Net;
+
+
+namespace flashcards_server.Connection
+{
+    
+    static class Connection
+    {
+        
+    }
+}
