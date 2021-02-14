9785509d41e9f35f4eef6dffdb5d83d81793c3be
B131R1N1I <mateuszbernart@gmail.com>
2021-02-14 19:31:51 +0100

    ReciveData
    Connecetion class contains class which recives data

--- flashcards_server/src/Connection/Connection.cs
+++ flashcards_server/src/Connection/Connection.cs
@@ -7,9 +7,28 @@ using System.Net;
 
 namespace flashcards_server.Connection
 {
-    
-    static class Connection
+
+    public class Connection
     {
-        
+        public Connection(string iPAddress, Int32 port)
+        {
+            server = new TcpListener(IPAddress.Parse(iPAddress), port);
+            server.Start();
+            System.Console.WriteLine($"Server: ip {iPAddress}, port: {port}");
+        }
+
+        public string ReciveData()
+        {
+            var cl = server.AcceptTcpClient();
+            var stream = cl.GetStream();
+            var buffer = new Byte[256];
+            var data = stream.Read(buffer);
+            stream.Close();
+            cl.Close();
+            return System.Text.Encoding.UTF8.GetString(buffer, 0, data);
+        }
+
+        TcpListener server = null;
+
     }
 }
