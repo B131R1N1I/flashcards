e3743457c7de5263144a1cf58ad58fc7d016533d
B131R1N1I <mateuszbernart@gmail.com>
2021-02-19 01:58:47 +0100

    CORS configured
    Thanks @morswin22

--- flashcards_server/src/Controllers/Controller.User.cs
+++ flashcards_server/src/Controllers/Controller.User.cs
@@ -17,19 +17,16 @@ namespace flashcards_server.API.Controllers
         [Route("login/{login}/{password}")]
         public string Get(string login, string password)
         {
-            // db.OpenConnection();
             try
             {
                 var u = db.GetUserByUsername(login);
                 var correct = db.PasswordMatch(u, password);
-                // db.CloseConnection();
                 return $"Login: {login}\n" +
                        $"Password: {password}\n" +
                        $"Correct: {correct}";
             }
             catch (Npgsql.NpgsqlException e)
             {
-                // db.CloseConnection();
                 return e.Message;
             }
 
@@ -40,8 +37,8 @@ namespace flashcards_server.API.Controllers
         [Route("register/")]
         [EnableCors]
         [Consumes("application/json")]
-        // [Produces("application/json")]
-        public SuccessMessage Register([FromBody] User.User u)
+        [Produces("application/json")]
+        public SuccessMessage Register(User.User u)
         {
             try
             {
--- flashcards_server/src/Startup.cs
+++ flashcards_server/src/Startup.cs
@@ -23,23 +23,13 @@ namespace flashcards_server.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // services.AddCors(options =>
-            // {
-            //     options.AddPolicy(name: MyAllowSpecificOrigins,
-            //                       builder =>
-            //                       {
-            //                           builder.WithOrigins("https://localhost:5001/")
-            //                                                 .AllowAnyHeader()
-            //                                                 .AllowAnyMethod();
-            //                       });
-            // });
             services.AddCors(options =>
             {
-                // this defines a CORS policy called "default"
                 options.AddPolicy("default", builder =>
                 {
-                    builder.WithOrigins("https://localhost:5001/*")
-                           .AllowAnyHeader();
+                    builder.WithOrigins("*")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
                 });
             });
             services.AddControllers();
