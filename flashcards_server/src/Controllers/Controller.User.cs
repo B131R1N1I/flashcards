c598906dcb2d7af16a06aad2cf15fd4e3c600fdc
B131R1N1I <mateuszbernart@gmail.com>
2021-02-23 15:29:17 +0100

    formatted exceptions

--- flashcards_server/src/Controllers/Controller.User.cs
+++ flashcards_server/src/Controllers/Controller.User.cs
@@ -14,26 +14,6 @@ namespace flashcards_server.API.Controllers
     [Route("fc/user")]
     public class LoginController : ControllerBase
     {
-        // [HttpGet]
-        // [Route("login/{login}/{password}")]
-        // public string Get(string login, string password)
-        // {
-        //     try
-        //     {
-        //         var u = db.GetUserByUsername(login);
-        //         var correct = db.PasswordMatch(u, password);
-        //         return $"Login: {login}\n" +
-        //                $"Password: {password}\n" +
-        //                $"Correct: {correct}";
-        //     }
-        //     catch (Npgsql.NpgsqlException e)
-        //     {
-        //         return e.Message;
-        //     }
-
-
-        // }
-
         [HttpPost]
         [Route("register/")]
         [EnableCors]
@@ -49,13 +29,11 @@ namespace flashcards_server.API.Controllers
                 var mess = new SuccessMessageResponseMessage { successed = true };
                 return new SuccessMessageResponseMessage { successed = true };
             }
-            catch (Npgsql.NpgsqlException e)
+            catch (Exception e)
             {
-                return new SuccessMessageResponseMessage { successed = false, reason = e.Message };
-            }
-            catch (FormatException e)
-            {
-                return new SuccessMessageResponseMessage { successed = false, reason = e.Message };
+                if (e is FormatException || e is Npgsql.NpgsqlException)
+                    return new SuccessMessageResponseMessage { successed = false, reason = e.Message };
+                throw;
             }
         }
 
@@ -70,9 +48,12 @@ namespace flashcards_server.API.Controllers
             {
                 var user = db.GetUserById(updateRequest.id);
                 var to = updateRequest.to;
-                switch (updateRequest.what.ToLower())
+                var what = updateRequest.what;
+                switch (what.ToLower())
                 {
                     case "email":
+                        if (!db.IsValidEmail(to))
+                            throw new FormatException($"{to} isn't correct email format.");
                         user.email = to;
                         break;
                     case "name":
@@ -85,18 +66,16 @@ namespace flashcards_server.API.Controllers
                         user.password = to;
                         break;
                     default:
-                        return new SuccessMessageResponseMessage() { successed = false };
+                        return CreateSuccessMessageResponseMessage(HttpStatusCode.BadRequest, false, $"'{what}' is not valid property");
 
                 }
                 return new SuccessMessageResponseMessage() { successed = true };
             }
-            catch (Npgsql.NpgsqlException e)
+            catch (Exception e)
             {
-                return new SuccessMessageResponseMessage() { successed = false, reason = e.Message };
-            }
-            catch (FormatException e)
-            {
-                return new SuccessMessageResponseMessage() { successed = false, reason = e.Message };
+                if (e is FormatException || e is Npgsql.NpgsqlException)
+                    return CreateSuccessMessageResponseMessage(HttpStatusCode.BadRequest, false, e.Message);
+                throw;
             }
         }
 
@@ -126,20 +105,34 @@ namespace flashcards_server.API.Controllers
         }
 
         [HttpGet]
-        [Route("isEmailUnique")]
+        [Route("isEmailAlreadyUsed")]
         [EnableCors]
         [Produces("application/json")]
-        public HttpResponseMessage isEmailUnique(string email)
+        public HttpResponseMessage isEmailUsed(string email)
         {
             if (db.IsValidEmail(email))
                 return new IsAleradyUsedResponseMessage() { isAlreadyUsed = !db.IsUserEmailUnique(email) };
             else
-                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                return CreateSuccessMessageResponseMessage(HttpStatusCode.BadRequest, false, $"{email} isn't correct email format.");
+        }
+
+        [HttpGet]
+        [Route("isUsernameAlreadyUsed")]
+        [EnableCors]
+        [Produces("application/json")]
+        public HttpResponseMessage isUsernameUsed(string username)
+        {
+            return new IsAleradyUsedResponseMessage() { isAlreadyUsed = !db.IsUserUsernameUnique(username) };
         }
 
         private PublicUserResponseMessage CreatePublicUserResponseMessage(User.User u)
         {
             return new PublicUserResponseMessage { id = u.id, username = u.username };
+        }
+
+        private SuccessMessageResponseMessage CreateSuccessMessageResponseMessage(HttpStatusCode code, bool successed, string message)
+        {
+            return new SuccessMessageResponseMessage() { StatusCode = code, successed = successed, reason = message };
         }
 
         DatabaseManagement.DatabaseManagement db = flashcards_server.Program.db;
