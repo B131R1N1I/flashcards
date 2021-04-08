6c572708b2f82cf7a9d79731ac73e871274c2de2
B131R1N1I <mateuszbernart@gmail.com>
2021-04-08 22:18:07 +0200

    Controller.Set uses authentication

---
+++ flashcards_server/src/Controllers/Controller.Set.SetToCreate.cs
@@ -0,0 +1,8 @@
+namespace flashcards_server.Controllers
+{
+    public class SetToCreate
+    {
+        public string name { get; set; }
+        public bool isPublic { get; set; }
+    }
+}
--- flashcards_server/src/Controllers/Controller.Set.cs
+++ flashcards_server/src/Controllers/Controller.Set.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
@@ -9,32 +11,33 @@ using Microsoft.AspNetCore.Mvc;
 namespace flashcards_server.Controllers
 {
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("fc/set")]
     public class SetController : ControllerBase
     {
         [HttpPost]
         [Route("create")]
         [EnableCors]
-        [Authorize]
         [Consumes("application/json")]
         [Produces("application/json")]
-        public SuccessMessageResponseMessage CreateSet(MinSet minSet)
+        public IActionResult CreateSet(SetToCreate setToCreate)
         {
-            var set = CreateSetFromMinSet(minSet);
             try
             {
                 using (var context = new flashcardsContext())
                 {
+                    var id = LoggedInId();
+                    var set = new Set.Set(setToCreate.name, id, id, setToCreate.isPublic);
                     context.sets.Add(set);
                     context.SaveChanges();
                 }
 
-                System.Console.WriteLine($">>> ADDED SET {set.name}");
-                return new SuccessMessageResponseMessage(true);
+                System.Console.WriteLine($">>> ADDED SET {setToCreate.name}");
+                return Ok();
             }
-            catch (Npgsql.NpgsqlException e)
+            catch (Exception e)
             {
-                return new SuccessMessageResponseMessage(false, e.Message, HttpStatusCode.BadRequest);
+                return BadRequest(e.Message);
             }
         }
 
@@ -44,8 +47,8 @@ namespace flashcards_server.Controllers
         [Produces("application/json")]
         public List<Set.Set> GetAllSets()
         {
-            using (var context = new flashcardsContext())
-                return context.sets.Where(s => s.isPublic == true).ToList();
+            using var context = new flashcardsContext();
+            return context.sets.Where(s => s.isPublic).ToList();
         }
 
         [HttpGet]
@@ -55,15 +58,16 @@ namespace flashcards_server.Controllers
         public Set.Set GetSetById(uint id)
         {
             using var context = new flashcardsContext();
-                return context.sets.First(s => s.id == id);
+            return context.sets.First(s => s.id == id);
         }
 
         [HttpGet]
         [Route("getSetsForMe")]
         [EnableCors]
         [Produces("application/json")]
-        public List<Set.Set> GetSetsAvailableForUser(uint id)
+        public List<Set.Set> GetSetsAvailableForUser()
         {
+            var id = LoggedInId();
             using var context = new flashcardsContext();
             return context.sets.Where(s => s.ownerId == id || s.creatorId == id).ToList();
         }
@@ -85,27 +89,35 @@ namespace flashcards_server.Controllers
         [EnableCors]
         [Consumes("application/json")]
         [Produces("application/json")]
-        public SuccessMessageResponseMessage TransferOwnership(ChangeOwnership change)
+        public IActionResult TransferOwnership(ChangeOwnership change)
         {
             try
             {
                 using var context = new flashcardsContext();
                 
-                var u = context.users.First(u => u.Id == change.userId); 
-                // db.GetUserById(change.userId);
-                var s = context.sets.First(s => s.id == change.setId);
-                // db.GetSetById(change.setId);
-                s.ownerId = u.Id;
-                // db.TransferOwnership(s, u);
-                return new SuccessMessageResponseMessage(true);
+                var set = context.sets.First(s => s.id == change.setId);
+                if (set.ownerId != LoggedInId())
+                    return Unauthorized("Access denied for this set");
+                
+                var user = context.users.First(u => u.Id == change.userId);
+
+                set.ownerId = user.Id;
+                context.SaveChanges();
+                
+                return Ok();
             }
             catch (ArgumentNullException e)
             {
-                return new SuccessMessageResponseMessage(false, e.Message, HttpStatusCode.BadRequest);
+                return BadRequest(e.Message);
             }
         }
-
-
+        
+        private int LoggedInId()
+        {
+            return int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? 
+                             throw new InvalidOperationException(
+                                 $"Cannot validate - there's no user with id {ClaimTypes.NameIdentifier}"));
+        }
         private Set.Set CreateSetFromMinSet(MinSet minSet)
         {
             return new Set.Set(minSet.name, minSet.creator, minSet.owner, minSet.isPublic);
